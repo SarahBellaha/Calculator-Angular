2dc8404f4ac90f96e5875c7074b04972a4db3d6e
BELLAHA Sarah (SNCF VOYAGEURS / Etablissement Gares Transilien Sud Ligne C / TERR JUINE ET ORGE) <9008080s@commun.ad.sncf.fr>
2022-10-24 16:51:43 +0200

    upload fichiers

---
+++ src/app/calculatrice-component/calculatrice-component.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculatrice-component',
+  templateUrl: './calculatrice-component.component.html',
+  styleUrls: ['./calculatrice-component.component.css']
+})
+export class CalculatriceComponentComponent implements OnInit {
+
+  result: number | string = 0;
+  firstValue: Array<number | string> = [];
+  secondValue: Array<number | string> = [];
+  operator: string | null = null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  reset():void {
+    this.result = 0;
+    this.firstValue = [];
+    this.secondValue = [];
+    this.operator = null;
+  }
+
+  calculate():void {
+    switch(this.operator) {
+      case "+":
+        this.result = Number(this.firstValue.join("")) + Number(this.secondValue.join(""));
+      break;
+
+      case "-":
+        this.result = Number(this.firstValue.join("")) - Number(this.secondValue.join(""));
+      break;
+
+      case "/":
+        this.result = Number(this.firstValue.join("")) / Number(this.secondValue.join(""));
+      break;
+
+      case "*":
+        this.result = Number(this.firstValue.join("")) * Number(this.secondValue.join(""));
+      break;
+
+      // case "%":
+      //   if(this.operator !== null) {
+      //     this.result = Number(this.firstValue.join("")) * Number(this.secondValue.join(""));
+      //   }
+        
+    }
+  }
+
+  getValue(value: number | string): void {
+    if(this.operator === null) {
+      this.firstValue.push(value);
+      this.result = this.firstValue.join("");
+    } else if(this.operator !== null) {
+      this.secondValue.push(value);
+      this.result = this.secondValue.join("");
+    }
+  }
+
+  getOperator(operator: string): void {
+    this.operator = operator;
+  }
+
+  displayValue(): void {
+    this.calculate();
+  }
+}
